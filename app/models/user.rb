584d3a4972fba1296eb758d1e451e8317e4aecc7
Roli <rlkhattri@gmail.com>
2018-08-10 08:27:52 +0000

    remove validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :contact, :numericality => true,
-                 :length => { :is => 10, :message => "Enter a 10 digit contact number" }
 end
